713630569390f84bb73b4bf47851875018cfaaca
Alvin Setiawan <alvin.set25@gmail.com>
2025-02-15 17:54:38 +0700

    feat(jest): add module name mapper

--- jest.config.js
+++ jest.config.js
@@ -89,7 +89,11 @@ module.exports = {
 	// ],
 
 	// A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-	// moduleNameMapper: {},
+	moduleNameMapper: {
+    "@config": "<rootDir>/src/config",
+    "@main": "<rootDir>/src/main",
+    "@utils": "<rootDir>/src/utils"
+  },
 
 	// An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
 	// modulePathIgnorePatterns: [],
